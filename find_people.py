dfc9bb2ecef7b8738b2c62ea0c6d857d2d08264a
Guofu Li <li.guofu.l@gmail.com>
2015-01-14 18:51:37 +0000

    updates..

--- find_people.py
+++ find_people.py
@@ -2,10 +2,33 @@ import pickle as cp
 import csv
 import random as rand
 
+category2genre = {
+	'entertainment' : ['movies','television','music','theatre','art','singing','cartoons','comedy','comics','magic','pornography','science fiction','stunts','video games','radio'], 
+	'social_change' : ['politics','law','crime','espionage'],
+	'business' : ['economics','business','finance'],
+	'tech' : ['technology', 'aeronautics'],
+	'education' : ['philosophy','literature', 'education'],
+}
+
+genre2category = {
+	'aeronautics': 'tech', 'video games': 'entertainment', 'art': 'entertainment', 'radio': 'entertainment', 
+	'pornography': 'entertainment', 'politics': 'social_change', 'singing': 'entertainment', 'theatre': 'entertainment', 
+	'science fiction': 'entertainment', 'technology': 'tech', 'crime': 'criminal', 'music': 'entertainment', 
+	'medicine': 'education', 'comedy': 'entertainment', 'espionage': 'criminal', 'literature': 'education', 
+	'finance': 'social_change', 'business': 'social_change', 'philosophy': 'education', 'law': 'social_change', 
+	'tragedy': 'criminal', 'stunts': 'entertainment', 'television': 'entertainment', 'magic': 'entertainment', 
+	'comics': 'entertainment', 'economics': 'social_change', 'movies': 'entertainment', 'exploration': 'tech', 
+	'cartoons': 'entertainment', 'history': 'social_change', 'education' : 'education'
+}
+
+genre_category_oppo = {
+	'business' : 'entertainment',
+	'social_change' : 'business',
+}
 
 def guofu_load_data():
 	global all_names, all_genre, oppo_map, all_name_set, people_has_oppo
-	print 'will load data'
+#	print 'will load data'
 	f = open('names.pk', 'r')
 	all_names = cp.load(f)
 	f.close()
@@ -14,7 +37,7 @@ def guofu_load_data():
 	all_genre = cp.load(f)
 	f.close()
 
-	f = open('oppo_map.pk', 'r')
+	f = open('oppo_map_INKB.pk', 'r')
 	oppo_map = cp.load(f)
 	f.close()
 
@@ -45,7 +68,7 @@ def load_from_csv():
 
 
 def make_genre_to_names():
-	f = open('all_genre.pk', 'wb')
+	f = open('all_genre.pk', 'w')
 	all_genre = {}
 
 	for name in names:
@@ -61,13 +84,15 @@ def make_genre_to_names():
 	f.close()
 
 def make_oppo_dict():
-	f = open('oppo_map.pk', 'wb')
+	f = open('oppo_map_INKB.pk', 'w')
 	oppo_dict = {}
 
 	for name in names:
 		oppo_list = names[name]['Opponent'].split(',')
 		for oppo in oppo_list:
 			oppo = oppo.strip()
+			if len(oppo) < 2 or oppo not in names:
+				continue
 			if name not in oppo_dict:
 				oppo_dict[name] = set([oppo])
 			else:
@@ -90,41 +115,58 @@ people_has_oppo = None
 
 guofu_load_data()
 
+def people_in_genres(genre_list):
+	people_list = []
+	for genre in genre_list:
+		people_list += all_genre[genre]
+	return set(people_list)
+
 def find_people(genre):
-	global all_genre, all_names, oppo_map, all_name_set, people_has_oppo
+	global all_genre, all_names, oppo_map, all_name_set
+	global genre2category, category2genre, people_has_oppo
 	
 	if genre not in all_genre:
 		print 'Warning: there is no such genre as', genre
 		return None
 
+	genre_category = genre2category[genre];
 	people_of_genre = all_genre[genre]
-	if len(people_of_genre) == 1:
-		print 'Warning: there is only %d people in genre %s' % (len(people_of_genre), genre)
+	neighbor_genres = category2genre[genre_category]
+	using_big_category = False
+
+	# if there are two few people in the small genre, then use the big category
+	if len(people_of_genre) < 2:
+		print 'Warning: there is only %d people in genre %s, trying to using the super category' % (len(people_of_genre), genre)
+		people_of_genre = people_in_genres(neighbor_genres)
+		using_big_category = True
+
+	# if still the case, then nothing we can do...
+	if len(people_of_genre) < 2:
 		first_people = rand.sample(people_of_genre, 1)[0]
 		second_people = rand.sample(all_name_set, 1)[0]
-	elif len(people_of_genre) == 0:
-		print 'Warning: there is no people of this genre %s' % genre
-		[first_people, second_people] = rand.sample(all_name_set, 2)
 	else:
+		# people in genre with opponents
 		people_of_genre_has_oppo = people_of_genre & people_has_oppo
-		if len(people_of_genre_has_oppo) < 2:
-			print 'Warning: no people in genre %s has opponent' % genre
-			(first_people, second_people) = rand.sample(people_of_genre, 2)
+		# if there is no people with opponents...
+		if len(people_of_genre_has_oppo) < 1:
+			# if we already used the big category
+			if using_big_category == True:
+				print 'Warning: no people in genre %s has opponent, trying to find a people from opposite genre category of %s' % (genre, genre_category)
+				(first_people, second_people) = rand.sample(people_of_genre, 2)
+				return (first_people, second_people)
+			else:
+				# otherwise, try to use the big category
+				people_of_genre = people_in_genres(neighbor_genres)
+				people_of_genre_has_oppo = people_of_genre & people_has_oppo
+				if len(people_of_genre_has_oppo) < 1:
+					# if still has this trouble...
+					print 'Warning: no people in genre %s has opponent, trying to find a people from opposite genre category of %s' % (genre, genre_category)
+					(first_people, second_people) = rand.sample(people_of_genre, 2)
+					return (first_people, second_people)
 		else:
 			first_people = rand.sample(people_of_genre_has_oppo, 1)[0]
 			second_people_cand_list = set(oppo_map[first_people])
-			print 'did get 2nd people candidates', str(second_people_cand_list)
+			# print 'did get 2nd people candidates', str(second_people_cand_list)
 			second_people = rand.sample(second_people_cand_list, 1)[0]
-	
-	print 'did find 1st people', first_people, '2nd people', second_people
-
-	if second_people not in all_name_set:
-		print '2nd name %s is not in the KB' % second_people
-		second_people_info = {}
-	else:
-		second_people_info = all_names[second_people]
-	first_people_info = all_names[first_people]
-
-	return ({first_people: first_people_info}, {second_people: second_people_info})
-
+	return first_people, second_people
 
