c42b14b88a3b8e836a31b5219371525716099f46
Guofu Li <li.guofu.l@gmail.com>
2015-01-14 13:33:17 +0000

    some bug fixed...

--- find_people.py
+++ find_people.py
@@ -2,50 +2,6 @@ import pickle as cp
 import csv
 import random as rand
 
-all_names = None
-all_genre = None
-oppo_map = None
-
-all_name_set = None
-people_has_oppo = None
-
-guofu_load_data()
-
-def find_people(genre):
-	global all_genre, all_names, oppo_map, all_name_set, people_has_oppo
-	
-	if genre not in all_genre:
-		print 'Warning: there is no such genre as', genre
-		return None
-
-	people_of_genre = all_genre[genre]
-	if len(people_of_genre) == 1:
-		print 'Warning: there is only %d people in genre %s' % (len(people_of_genre), genre)
-		first_people = rand.sample(people_of_genre, 1)[0]
-		second_people = rand.sample(all_name_set, 1)[0]
-	elif len(people_of_genre) == 0:
-		print 'Warning: there is no people of this genre %s' % genre
-		[first_people, second_people] = rand.sample(all_name_set, 2)
-	else:
-		people_of_genre_has_oppo = people_of_genre & people_has_oppo
-		if len(people_of_genre_has_oppo) < 2:
-			print 'Warning: no people in genre %s has opponent' % genre
-			(first_people, second_people) = rand.sample(people_of_genre, 2)
-		else:
-			first_people = rand.sample(people_of_genre_has_oppo, 1)[0]
-			second_people_cand_list = set(oppo_map[first_people])
-			print 'did get 2nd people candidates', str(second_people_cand_list)
-			second_people = rand.sample(second_people_cand_list, 1)[0]
-	
-	print 'did find 1st people', first_people, '2nd people', second_people
-
-	if second_people not in all_name_set:
-		print '2nd name %s is not a people' % second_people
-	second_people_info = all_names[second_people]
-	first_people_info = all_names[first_people]
-
-	return ({first_people: first_people_info}, {second_people: second_people_info})
-
 
 def guofu_load_data():
 	global all_names, all_genre, oppo_map, all_name_set, people_has_oppo
@@ -123,3 +79,52 @@ def make_oppo_dict():
 
 	cp.dump(oppo_dict, f)
 	f.close()
+
+	
+all_names = None
+all_genre = None
+oppo_map = None
+
+all_name_set = None
+people_has_oppo = None
+
+guofu_load_data()
+
+def find_people(genre):
+	global all_genre, all_names, oppo_map, all_name_set, people_has_oppo
+	
+	if genre not in all_genre:
+		print 'Warning: there is no such genre as', genre
+		return None
+
+	people_of_genre = all_genre[genre]
+	if len(people_of_genre) == 1:
+		print 'Warning: there is only %d people in genre %s' % (len(people_of_genre), genre)
+		first_people = rand.sample(people_of_genre, 1)[0]
+		second_people = rand.sample(all_name_set, 1)[0]
+	elif len(people_of_genre) == 0:
+		print 'Warning: there is no people of this genre %s' % genre
+		[first_people, second_people] = rand.sample(all_name_set, 2)
+	else:
+		people_of_genre_has_oppo = people_of_genre & people_has_oppo
+		if len(people_of_genre_has_oppo) < 2:
+			print 'Warning: no people in genre %s has opponent' % genre
+			(first_people, second_people) = rand.sample(people_of_genre, 2)
+		else:
+			first_people = rand.sample(people_of_genre_has_oppo, 1)[0]
+			second_people_cand_list = set(oppo_map[first_people])
+			print 'did get 2nd people candidates', str(second_people_cand_list)
+			second_people = rand.sample(second_people_cand_list, 1)[0]
+	
+	print 'did find 1st people', first_people, '2nd people', second_people
+
+	if second_people not in all_name_set:
+		print '2nd name %s is not in the KB' % second_people
+		second_people_info = {}
+	else:
+		second_people_info = all_names[second_people]
+	first_people_info = all_names[first_people]
+
+	return ({first_people: first_people_info}, {second_people: second_people_info})
+
+
