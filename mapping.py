6f8ee2319e72b47bfbad14aab0661ff856abc128
Dora Lee Borg <doralborg@gmail.com>
2015-01-14 17:52:36 +0000

    fix 4

--- mapping.py
+++ mapping.py
@@ -5,7 +5,8 @@ from hashtags import popular_hashtags
 def mapping():
 	D = popular_hashtags()
 	D.pop('Astrology')
-	D.pop('Environment')	
+	D.pop('Environment')
+	D.pop('General')	
 	trending_category = str(random.choice(D.keys()))
 	
 	hashtag = str(random.choice(D[trending_category]))
@@ -13,8 +14,6 @@ def mapping():
 	entertainment = ['movies','television','music','theatre','art','singing','cartoons','comedy','comics','magic','pornography','science fiction','stunts','video games','radio']
 
 	social_change = ['politics','law','crime','espionage']
-	
-	general = ['history','exploration','tragedy','medicine']
 	
 	business = ['economics','business','finance']
 
@@ -27,9 +26,6 @@ def mapping():
 	
 	elif trending_category == 'Social Change':
 		genre = str(random.choice(social_change))
-		
-	elif trending_category == 'General':
-		genre = str(random.choice(general))
 	
 	elif trending_category == 'Business':
 		genre = str(random.choice(business))
@@ -39,8 +35,6 @@ def mapping():
 
 	elif trending_category == 'Education':
 		genre = str(random.choice(education))	
-	
-	print trending_category
 	
 	return (genre,hashtag)
 
